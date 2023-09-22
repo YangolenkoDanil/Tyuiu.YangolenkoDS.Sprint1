e2b793a488272d1ed68db20bcaac33c285492c47
Tyuiu.YangolenkoDS@outlook.com <angolenkodanil@gmail.com>
2023-09-22 19:21:09 +0500

    Task2

--- Tyuiu.YangolenkoDS.Sprint1.Task2.V2.Lib/DataService.cs
+++ Tyuiu.YangolenkoDS.Sprint1.Task2.V2.Lib/DataService.cs
@@ -10,11 +10,10 @@ namespace Tyuiu.YangolenkoDS.Sprint1.Task2.V2.Lib
 {
     public class DataService : ISprint1Task2V2
     {
-        public const double pi = 3.1415926535897932384626433832795;
 
         public double ConvertAngleToRad(int value)
         {
-            return value * (pi / 180);
+            return value * (Math.PI / 180);
         }
     }
 }
