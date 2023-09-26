7813d7bf199fea5ca07d6d2420e3fd8bc6578223
Tyuiu.YangolenkoDS@outlook.com <angolenkodanil@gmail.com>
2023-09-26 13:56:49 +0500

    Task5

---
+++ Tyuiu.YangolenkoDS.Sprint1.Task5.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.YangolenkoDS.Sprint1.Task5.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.YangolenkoDS.Sprint1.Task5.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b52a1e6-de27-4431-8e55-c03a587f154c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
