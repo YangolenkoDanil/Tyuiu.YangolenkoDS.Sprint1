02a7e98563b54762cb1cfd0e746adb7b0c112263
Tyuiu.YangolenkoDS@outlook.com <angolenkodanil@gmail.com>
2023-09-28 16:43:58 +0500

    Task6

--- Tyuiu.YangolenkoDS.Sprint1.Task6.V7.Lib/DataService.cs
+++ Tyuiu.YangolenkoDS.Sprint1.Task6.V7.Lib/DataService.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.YangolenkoDS.Sprint1.Task6.V7.Lib
         public string DeleteLastLetter(string value)
         {
             string[] words = value.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
-            string[] wordsRemoveLastLiter = words.Where(x => x.Length <= 30).Select(x => x.Remove(x.Length - 1)).ToArray<string>();
+            string[] wordsRemoveLastLiter = words.Select(x => x.Remove(x.Length - 1)).ToArray<string>();
             string strFinal = string.Join(" ", wordsRemoveLastLiter);
             return strFinal;
         }
